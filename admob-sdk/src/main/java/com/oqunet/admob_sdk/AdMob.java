477bee50753cb799fbea87cdb0d66c76854baaee
Mounzer <mounzer.orabi@gmail.com>
2018-11-15 18:26:17 +0200

    Initial Commit

--- admob-sdk/src/main/java/com/oqunet/admob_sdk/AdMob.java
+++ admob-sdk/src/main/java/com/oqunet/admob_sdk/AdMob.java
@@ -1,27 +1,17 @@
 package com.oqunet.admob_sdk;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.oqunet.admob_sdk.receiver.PhoneStateReceiver;
-import com.oqunet.admob_sdk.utils.AppUtils;
-import com.oqunet.admob_sdk.utils.PermissionsDelegate;
 
 public class AdMob {
     private static final String BROADCAST = "com.oqunet.admob_sdk.android.intent.action.PHONE_STATE";
 
 
-    public static void showAdAfterCall(Activity activity) {
-
+    public static void showAdAfterCall(Context context) {
 
         Intent intent = new Intent(BROADCAST);
-        activity.sendBroadcast(intent);
+        context.sendBroadcast(intent);
 
         /**
         IntentFilter intentFilter = new IntentFilter(BROADCAST);
